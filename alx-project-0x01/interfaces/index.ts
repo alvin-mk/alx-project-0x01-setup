7888888e18cbde69fbeb948224df1f9b63d42dd6
Alvin K <ak.mbuthia98@gmail.com>
2024-12-20 17:19:24 +0400

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,58 +1,62 @@
-// Interface for a Post
+// Interface for PostData
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+// Interface for PostProps
 export interface PostProps {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-  }
-  
-  // Interface for the PostModal component
-  export interface PostData {
-    userId: number;
-    id?: number;
-    title: string;
-    body: string;
-  }
-  
-  export interface PostModalProps {
-    onClose: () => void;
-    onSubmit: (post: PostData) => void;
-  }
-  
-  // Interface for a User
-  export interface Geo {
-    lat: string;
-    lng: string;
-  }
-  
-  export interface Address {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+// Interface for PostModalProps
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+
+// Interface for UserData
+export interface UserData {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
     street: string;
     suite: string;
     city: string;
     zipcode: string;
-    geo: Geo;
-  }
-  
-  export interface Company {
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
     name: string;
     catchPhrase: string;
     bs: string;
-  }
-  
-  export interface UserData {
-    id?: number;
-    name: string;
-    username: string;
-    email: string;
-    address: Address;
-    phone: string;
-    website: string;
-    company: Company;
-  }
-  
-  // Interface for the UserModal component
-  export interface UserModalProps {
-    onClose: () => void;
-    onSubmit: (user: UserData) => void;
-  }
-  
+  };
+}
+
+// Interface for UserProps
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+// Interface for UserModalProps
+export interface UserModalProps {
+  onClose: () => void;
+  onSubmit: (user: UserData) => void;
+}
