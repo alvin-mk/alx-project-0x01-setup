5071f333b24254c8c1a3ddc3fe919114f97ad68e
Alvin K <ak.mbuthia98@gmail.com>
2024-12-20 16:24:36 +0400

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import UserCard from "@/components/common/UserCard";
 import UserModal from "@/components/common/UserModal";
+import { PostProps }
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { UserData } from "@/interfaces";
