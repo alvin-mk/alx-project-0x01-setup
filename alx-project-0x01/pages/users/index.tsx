024476ad17bfdae288f094f564389a74a75621d8
Alvin K <ak.mbuthia98@gmail.com>
2024-12-20 17:13:28 +0400

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,17 +2,17 @@ import UserCard from "@/components/common/UserCard";
 import UserModal from "@/components/common/UserModal";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import { UserData } from "@/interfaces";
+import { UserData, PostProps } from "@/interfaces";
 import { useState } from "react";
 
-const Users: React.FC<{ users: UserData[] }> = ({ users }) => {
+const Users: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
   const [isModalOpen, setModalOpen] = useState(false);
-  const [userList, setUserList] = useState<UserData[]>(users);
+  const [userPosts, setUserPosts] = useState<PostProps[]>(posts);
 
-  const handleAddUser = (newUser: UserData) => {
-    setUserList((prevUsers) => [
-      ...prevUsers,
-      { ...newUser, id: prevUsers.length + 1 },
+  const handleAddPost = (newPost: PostProps) => {
+    setUserPosts((prevPosts) => [
+      ...prevPosts,
+      { ...newPost, id: prevPosts.length + 1 },
     ]);
   };
 
@@ -21,22 +21,28 @@ const Users: React.FC<{ users: UserData[] }> = ({ users }) => {
       <Header />
       <main className="p-4 flex-grow">
         <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-semibold">User List</h1>
+          <h1 className="text-2xl font-semibold">User Posts</h1>
           <button
             onClick={() => setModalOpen(true)}
             className="bg-blue-700 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
           >
-            Add User
+            Add Post
           </button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {userList.map((user) => (
-            <UserCard key={user.id} {...user} />
+          {userPosts.map(({ id, title, body, userId }) => (
+            <UserCard
+              key={id}
+              id={id}
+              title={title}
+              body={body}
+              userId={userId}
+            />
           ))}
         </div>
       </main>
       {isModalOpen && (
-        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddUser} />
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
       )}
       <Footer />
     </div>
@@ -44,12 +50,12 @@ const Users: React.FC<{ users: UserData[] }> = ({ users }) => {
 };
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: UserData[] = await response.json();
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts: PostProps[] = await response.json();
 
   return {
     props: {
-      users,
+      posts,
     },
   };
 }
